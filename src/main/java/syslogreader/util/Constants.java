8937d0d131fcf355a865064ba667940b29d0fc64
noname <nomail@mail.com>
2014-02-21 16:43:39 +0000

    Updated stat view files

--- src/main/java/syslogreader/util/Constants.java
+++ src/main/java/syslogreader/util/Constants.java
@@ -17,13 +17,11 @@ public class Constants {
             "var pattern = \"(\\\\w+\\\\.[a-z]{2,4})$\";\n" +
 "\n" +
 "function getDomain(domainStr) {\n" +
-"	//alert(\"Parameter: \"+domainStr);\n" +
 "	var regex = new RegExp(pattern,\"mg\");\n" +
 "	var resultArray = new Array();\n" +
 "	resultArray = regex.exec(domainStr);\n" +
 "	\n" +
 "	if(resultArray && resultArray.length>0) {\n" +
-"		//alert(\"util.js/getDomain says: \"+resultArray[0]);\n" +
 "		return resultArray[0];\n" +
 "	} else {\n" +
 "		console.error(\"No domain found for \"+domainStr);\n" +
@@ -33,7 +31,6 @@ public class Constants {
 "\n" +
 "function buildHashMapByIP(logArray) {\n" +
 "	var hashmap = {};\n" +
-"	\n" +
 "	if(logArray.length>0) {\n" +
 "		var ipLogsArr = new Array();\n" +
 "		ipLogsArr.push(logArray[0]);\n" +
@@ -57,41 +54,83 @@ public class Constants {
 "	}\n" +
 "}\n" +
 "\n" +
+"function get_random_color() {\n" +
+"	var colors = ['#428bca', '#5cb85c', '#5bc0de', '#f0ad4e', '#d9534f'];\n" +
+"	var randNo = Math.floor((Math.random()*5));;\n" +
+"	console.debug('randNo = '+randNo);\n" +
+"    //return color;\n" +
+"    return colors[randNo];\n" +
+"}\n" +
+"\n" +
 "function buildBrowsingHistoryPerIP(hashmap) {\n" +
+"	//$('#graphDiv').empty();\n" +
+"	\n" +
 "	var tabsDiv = $('<ul></ul>').attr('id','myTab');\n" +
 "	tabsDiv.attr('class','nav nav-tabs');\n" +
 "	var tabsCont = $('<div></div>').attr('id','myTabContent');\n" +
 "	tabsCont.attr('class','tab-content');\n" +
 "\n" +
-"	for(var key in hashmap) {\n" +
-"		console.log('IP: '+key);\n" +
-"		var ipData = buildBrowsHistPerIPData(hashmap[key]);\n" +
-"		var pieGraphDiv = drawGraphPerIP(key,ipData);\n" +
-"		\n" +
-"		// Drawing Tabs for each IP address\n" +
-"		var tabDiv = $('<li></li>');\n" +
-"		var tabLink = $('<a></a>');\n" +
-"		tabLink.attr('href','#'+key.replace(/\\./g, '_' ));\n" +
-"		tabLink.attr('data-toggle','tab');\n" +
-"		tabLink.append(key);\n" +
-"		tabDiv.append(tabLink);\n" +
-"		tabsDiv.append(tabDiv);\n" +
-"		\n" +
-"		// Filling tabs with content\n" +
-"		var tabsContDiv = $('<div></div>').attr('id', key.replace(/\\./g, '_' ));\n" +
-"		tabsContDiv.attr('class','tab-pane fade');\n" +
-"		tabsContDiv.append(pieGraphDiv);\n" +
-"		tabsCont.append(tabsContDiv);\n" +
-"		\n" +
-"		//console.log(hashmap[key]);\n" +
+"	for(var ip in hashmap) {\n" +
+"		console.log('IP: '+ip);\n" +
+"		tabsDiv.append(buildNewTabTitle(ip));\n" +
+"		tabsCont.append(fillTabWithContents(ip, hashmap[ip]));\n" +
+"		//console.log(hashmap[ip]);\n" +
 "	}\n" +
 "	\n" +
+"	bindActionsToTheGraph();\n" +
 "	$(\"#graphDiv\").append(tabsDiv);\n" +
 "	$(\"#graphDiv\").append(tabsCont);\n" +
 "}\n" +
 "\n" +
+"function buildNewTabTitle(ip) {\n" +
+"	// Drawing Tabs for each IP address\n" +
+"	var tabDiv = $('<li></li>');\n" +
+"	var tabLink = $('<a></a>');\n" +
+"	tabLink.attr('href','#'+ip.replace(/\\./g, '_' ));\n" +
+"	tabLink.attr('data-toggle','tab');\n" +
+"	//tabDiv.css(\"background-color\",get_random_color());\n" +
+"	tabLink.append(ip);\n" +
+"	tabDiv.append(tabLink);\n" +
+"	return tabDiv;\n" +
+"}\n" +
+"\n" +
+"function fillTabWithContents(ip, data) {\n" +
+"	// Filling tabs with content\n" +
+"	var h4Title = $('<H4></H4>').attr('class','graphTitle');\n" +
+"	h4Title.append('Browsing history for IP <span>'+ip+'</span> <small class=\"hover\"></small>');\n" +
+"	\n" +
+"	var ipData = buildBrowsHistPerIPData(data);\n" +
+"	var pieGraphDiv = drawGraphPerIP(ip,ipData);\n" +
+"	var tabsContDiv = $('<div></div>').attr('id', ip.replace(/\\./g, '_' ));\n" +
+"	tabsContDiv.attr('class','tab-pane fade');\n" +
+"	tabsContDiv.append(h4Title);\n" +
+"	tabsContDiv.append(pieGraphDiv);\n" +
+"	return tabsContDiv;\n" +
+"}\n" +
+"\n" +
+"function bindActionsToTheGraph() {\n" +
+"	$(\"#graphDiv\").bind(\"plothover\", function(event, pos, obj) {\n" +
+"		if (!obj) {\n" +
+"			$(\".hover\").empty();\n" +
+"			return;\n" +
+"		}\n" +
+"		var percent = parseFloat(obj.series.percent).toFixed(2);\n" +
+"		$(\".hover\").css({'color':obj.series.color});\n" +
+"		$(\".hover\").text(obj.series.label + \" (\" + percent + \"%)\");\n" +
+"	});\n" +
+"	\n" +
+"	\n" +
+"	$(\"#graphDiv\").bind(\"plotclick\", function(event, pos, obj) {\n" +
+"		if (!obj) {\n" +
+"			return;\n" +
+"		}\n" +
+"		percent = parseFloat(obj.series.percent).toFixed(2);\n" +
+"		alert(\"\"  + obj.series.label + \": \" + percent + \"%\");\n" +
+"	});\n" +
+"}\n" +
+"\n" +
 "function drawGraphPerIP(ip, ipData) {\n" +
-"	var pieGraphDiv = $('<div></div>').attr('id',ip+'_div');\n" +
+"	var pieGraphDiv = $('<div></div>').attr('id',ip.replace(/\\./g, '_' )+'_graph');\n" +
 "\n" +
 "	var data = [];\n" +
 "	for (var key in ipData) {\n" +
@@ -100,16 +139,13 @@ public class Constants {
 "			data: ipData[key]\n" +
 "		});\n" +
 "	}\n" +
-"	//console.log(data.toSource());\n" +
-"	var ipDiv = $('<div></div>');\n" +
-"	//var ipDiv = $('<div></div>').attr('id',ip+'_div');\n" +
-"	var h4Title = $('<H4></H4>').attr('class','graphTitle');\n" +
-"	h4Title.append('Browsing history for IP <span>'+ip+'</span>');\n" +
-"	ipDiv.append(h4Title);\n" +
+"	\n" +
 "	var pieChartDiv = $('<div></div>').attr('class','graph');\n" +
 "	pieChartDiv.css('width','440px');\n" +
 "	pieChartDiv.css('height','240px');\n" +
+"	\n" +
 "	$(\"#graphDiv\").append(pieChartDiv);\n" +
+"	\n" +
 "	$.plot(pieChartDiv, data, {\n" +
 "    series: {\n" +
 "        pie: {\n" +
@@ -122,11 +158,28 @@ public class Constants {
 "    }\n" +
 "	});\n" +
 "	\n" +
-"	pieGraphDiv.append(ipDiv);\n" +
 "	pieGraphDiv.append(pieChartDiv);\n" +
-"	//$(\"#graphDiv\").append(ipDiv);\n" +
-"	//$(\"#graphDiv\").append(pieChartDiv);\n" +
 "	return pieGraphDiv;\n" +
+"}\n" +
+"\n" +
+"function updateGraphOnlyForIP(ip, rawData) {\n" +
+"	 // var domEl = document.getElementById(ip+'_div');\n" +
+"	 var ipStr = ip.replace(/\\./g, '_' );\n" +
+"	 //alert(\"ip [\"+ipStr+\"]\");\n" +
+"	//var domEl = $('#myTabContent').find(ipStr);\n" +
+"	var domEl = document.getElementById(ipStr+'_graph');\n" +
+"	\n" +
+"	document.write(domEl.html());\n" +
+"	// var domEl = $('#myTabContent');\n" +
+"	\n" +
+"\n" +
+"	//var arr = $('div > span').parent();\n" +
+"\n" +
+"\n" +
+"	//alert(domEl);\n" +
+"	console.debug( domEl.get() );\n" +
+"	domEl.show();\n" +
+"	domEl.remove();\n" +
 "}\n" +
 "\n" +
 "function buildBrowsHistPerIPData(browsHistArray) {\n" +
@@ -201,12 +254,12 @@ public class Constants {
 "\n" +
 "	<h1 class = \"masterhead\" >Browsing History read from the Syslog <small>file <span id=fileName></span></small></h1>\n" +
 "	<br />\n" +
-"	<button id=\"refreshButton\" type=\"button\" class=\"btn btn-primary\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Tooltip on left\">Refresh Data from the file</button>\n" +
+"	<button id=\"refreshButton\" type=\"button\" class=\"btn btn-primary\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Pulling data from the file\">Refresh Data from the file</button>\n" +
 "	<br />\n" +
 "		\n" +
 "	<div class=\"panel panel-default\">\n" +
 "	  <div class=\"panel-heading\"><h4 id=\"graphPanelTitle\">Graph View <small>as at <span class=curDateTime></span></small></h4></div>\n" +
-"	  <div class=\"panel-body\">\n" +
+"		<div class=\"panel-body\">\n" +
 "			<div id=graphDiv></div>\n" +
 "		</div>\n" +
 "	</div>\n" +
@@ -241,6 +294,8 @@ public class Constants {
     public String getTableStatFileCntx() {
         this.tableStatFileCntx =
             "var dateStr = new Date().toString();\n" +
+"var url = '"+this.getTxtfile()+"';\n" +
+"var browsHistHashMap = {};\n" +
 "\n" +
 "function LogData(date, time, localIP, destDomain) {\n" +
 "	this.date=date;\n" +
@@ -250,22 +305,27 @@ public class Constants {
 "}\n" +
 "\n" +
 "$(document).ready(function() {\n" +
+"	$(\".curDateTime\").append(dateStr.slice(3,24));\n" +
+"	$(\"#fileName\").append(url);\n" +
+"\n" +
 "	// Initialize tooltip\n" +
 "	$(function () {\n" +
 "        $(\"[data-toggle='tooltip']\").tooltip();\n" +
 "    });\n" +
+"	\n" +
 "	// Initialize tabs\n" +
 "	$('#myTab a').click(function (e) {\n" +
 "		e.preventDefault()\n" +
 "		$(this).tab('show')\n" +
 "	});\n" +
+"	\n" +
 "	// Initialize popover\n" +
 "	$(\"#graphPanelTitle\").popover({ title: 'Graph View', content: \"Click on the IP address to see its graph\", placement: 'top', trigger: 'hover' });\n" +
 "	$(\"#tablePanelTitle\").popover({ title: 'Table View', content: \"Here you can find all the _UNSORTED_ data\", placement: 'top', trigger: 'hover' });\n" +
 "	$(\"#graphPanelTitle\").css('display','inline-block');\n" +
 "	$(\"#tablePanelTitle\").css('display','inline-block');\n" +
 "	\n" +
-"	readTheDataFile('"+this.getTxtfile()+"');\n" +
+"	readTheDataFile(url);\n" +
 "	\n" +
 "	$(\"#refreshButton\").css('margin','10px');\n" +
 "	$(\"#refreshButton\").css('text-align','right');\n" +
@@ -273,30 +333,83 @@ public class Constants {
 "	$(\".masterhead\").css('text-align','center');\n" +
 "	//$(\".panel-body\").css('background-color','#eee');\n" +
 "	\n" +
+"	$(\"#refreshButton\").click(function(){\n" +
+"		//readTheDataFile(url);\n" +
+"		updateData();\n" +
+"	});\n" +
+"	\n" +
+"	\n" +
+"	\n" +
+"	// Add the Flot version string to the footer\n" +
+"	var flotVerP = $('<p></p>').attr('id','flot_ver');\n" +
+"	flotVerP.prepend(\"Flot \" + $.plot.version + \" &ndash; \");\n" +
+"	$(\"footer\").append(flotVerP);\n" +
 "});\n" +
 "\n" +
-"function readTheDataFile(url) {\n" +
-"	\n" +
+"function updateData() {\n" +
 "	var logArray = new Array();\n" +
-"	$(\".curDateTime\").append(dateStr.slice(3,24));\n" +
-"	$(\"#fileName\").append(url);\n" +
 "	\n" +
 "	$.ajax(url, {\n" +
 "		dataType: 'text',\n" +
 "		success: function (data) {\n" +
 "			var logStr = data.slice(0,data.length-3); 	// deleting the last ','\n" +
 "			logStr = '['+logStr+']';					// putting it within brackets\n" +
-"			//console.log(testing);\n" +
 "			\n" +
 "			$.each($.parseJSON(logStr), function(idx, obj) {\n" +
 "				var log = new LogData(obj.date, obj.time, obj.localIP, obj.destDomain);\n" +
-"				//console.log(log.toSource());\n" +
 "				logArray.push(log);\n" +
 "			});\n" +
 "			\n" +
-"			//displayArrayLength(logArray);\n" +
+"			// Update Table\n" +
+"			\n" +
 "			createTable(logArray);\n" +
-"			//getDomain('edge-star-shv-13-frc1.facebook.com');\n" +
+"			\n" +
+"			// Update Graph\n" +
+"			\n" +
+"			var hashmap = {};\n" +
+"			hashmap = buildHashMapByIP(logArray);\n" +
+"			\n" +
+"			for(var ip in hashmap) {\n" +
+"				if(browsHistHashMap[ip]) {\n" +
+"					console.debug('IP already exists, updating the Graph');\n" +
+"					updateGraphOnlyForIP(ip,hashmap[ip]);\n" +
+"				} else {\n" +
+"					console.debug('IP not found, adding new tab');\n" +
+"					updateTabsAndGraph(ip,hashmap[ip]);\n" +
+"				}\n" +
+"			}\n" +
+"		}\n" +
+"	});\n" +
+"}\n" +
+"\n" +
+"\n" +
+"function readTheDataFile(url) {\n" +
+"	var logArray = new Array();\n" +
+"	\n" +
+"	$.ajax(url, {\n" +
+"		dataType: 'text',\n" +
+"		success: function (data) {\n" +
+"			var logStr = data.slice(0,data.length-3); 	// deleting the last ','\n" +
+"			logStr = '['+logStr+']';					// putting it within brackets\n" +
+"			\n" +
+"			$.each($.parseJSON(logStr), function(idx, obj) {\n" +
+"				var log = new LogData(obj.date, obj.time, obj.localIP, obj.destDomain);\n" +
+"				logArray.push(log);\n" +
+"			});\n" +
+"			\n" +
+"			// Draw Table\n" +
+"			\n" +
+"			createTable(logArray);\n" +
+"			\n" +
+"			// Draw Graph\n" +
+"			\n" +
+"			browsHistHashMap = buildHashMapByIP(logArray);\n" +
+"	\n" +
+"			if(browsHistHashMap) {\n" +
+"				buildBrowsingHistoryPerIP(browsHistHashMap);\n" +
+"			} else {\n" +
+"				alert(\"Sorry, something went seriously wrong!\");\n" +
+"			}\n" +
 "		}\n" +
 "	});\n" +
 "}\n" +
@@ -306,7 +419,9 @@ public class Constants {
 "}\n" +
 "\n" +
 "function createTable(logArray) {\n" +
-"	var hashmap = {};	\n" +
+"	var tableDiv = $(\"#tableDiv\");\n" +
+"	tableDiv.empty();\n" +
+"	\n" +
 "	var table = $('<table></table>').attr('id','logsTable');\n" +
 "	var thead = $('<thead></thead>');\n" +
 "	var row = $('<tr></tr>');\n" +
@@ -322,18 +437,6 @@ public class Constants {
 "	\n" +
 "	table.append(thead);\n" +
 "	\n" +
-"	hashmap = buildHashMapByIP(logArray);\n" +
-"	\n" +
-"	if(hashmap) {\n" +
-"		var browsHist = buildBrowsingHistoryPerIP(hashmap);\n" +
-"	} else {\n" +
-"		alert(\"Sorry, something went seriously wrong!\");\n" +
-"	}\n" +
-"	\n" +
-"	//console.log(hashmap.toSource());\n" +
-"	//alert(\"Match Found! \"+getDomain(\"asd-32.12afg.google.com\"));\n" +
-"	\n" +
-"	\n" +
 "	$.each(logArray, function(idx, log) {\n" +
 "		var row = $('<tr></tr>');\n" +
 "		var col1 = $('<td></td>').text(log.date);\n" +
@@ -347,16 +450,10 @@ public class Constants {
 "		table.append(row);\n" +
 "	});\n" +
 "	\n" +
-"	$(\"#tableDiv\").append(table);\n" +
-"	\n" +
+"	tableDiv.append(table);\n" +
 "	$('#logsTable').dataTable( {\n" +
 "        \"aaSorting\": [[ 1, \"desc\" ]]\n" +
 "    } );\n" +
-"	\n" +
-"	// Add the Flot version string to the footer\n" +
-"	var flotVerP = $('<p></p>').attr('id','flot_ver');\n" +
-"	flotVerP.prepend(\"Flot \" + $.plot.version + \" &ndash; \");\n" +
-"	$(\"footer\").append(flotVerP);\n" +
 "}";
         return this.tableStatFileCntx;
     }
